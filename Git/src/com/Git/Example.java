50eb6b6af5b303a665bce2c44ccc927de0b7a5bb
imsushantb <154342278+imsushantb@users.noreply.github.com>
2023-12-21 21:02:33 +0530

    Update Example.java

--- Git/src/com/Git/Example.java
+++ Git/src/com/Git/Example.java
@@ -4,5 +4,6 @@ public class Example {
     public static void main(String[] args) {
 		System.out.println("Git example");
 		System.out.println("");
+	        System.out.println("Entry from GitHub");
 	}
 }
